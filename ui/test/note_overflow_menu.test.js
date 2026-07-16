ba7ab615cf967eaaf5d081c401fe4401137c05b0
Marc Schlaeppi <marc.schlaeppi@mssp.basevision.ch>
2026-07-16 10:54:47 +0200

    Remove redundant note overflow menu

--- ui/test/note_overflow_menu.test.js
+++ ui/test/note_overflow_menu.test.js
@@ -4,10 +4,9 @@ import test from 'node:test';
 
 const templateUrl = new URL('../../source/app/blueprints/pages/case/templates/case_notes_v2.html', import.meta.url);
 
-test('note overflow menu always exposes revision history', async () => {
+test('note toolbar keeps its revision history control without a redundant overflow menu', async () => {
     const template = await readFile(templateUrl, 'utf8');
 
-    assert.match(template, /id="noteQuickActionsToggle"[^>]*data-toggle="dropdown"/);
-    assert.match(template, /id="note_quick_actions"[^>]*aria-labelledby="noteQuickActionsToggle"/);
-    assert.match(template, /onclick="load_note_revisions\(\);return false;"[^>]*>.*Revision history/s);
+    assert.match(template, /onclick="load_note_revisions\(\)"/);
+    assert.doesNotMatch(template, /noteQuickActionsToggle|note_quick_actions|fa-ellipsis-v/);
 });
